142b7631731de1b1f509671557ac3505591420b4
dazyfreez <74270036+dazyfreez@users.noreply.github.com>
2022-06-05 15:44:09 +0200

    -

---
+++ db/-- SQLite.sql
@@ -0,0 +1,3 @@
+-- SQLite
+--CREATE TABLE kalender (tag TEXT NOT NULL PRIMARY KEY, datum TEXT NOT NULL, termin TEXT NOT NULL, beschreibung TEXT NOT NULL);
+INSERT INTO kalender (tag, datum, termin, beschreibung) VALUES ('Montag', '01.01.2020', 'Termin', 'Beschreibung');
