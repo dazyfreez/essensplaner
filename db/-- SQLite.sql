10893326ea41a2beb4a37a960ee7cb2320ba8829
dazyfreez <74270036+dazyfreez@users.noreply.github.com>
2022-06-06 23:14:17 +0200

    -

--- db/-- SQLite.sql
+++ db/-- SQLite.sql
@@ -1,12 +1,10 @@
 -- SQLite
 CREATE TABLE essensplaner_db (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
+    name TEXT PRIMARY KEY NOT NULL,
     date TEXT,
     time TEXT,
     description TEXT,
     category TEXT,
     priority TEXT,
     status TEXT,
-    user TEXT
 );
