89db1641c02056b8306a46c9b688bdbcc7d9317d
dazyfreez <74270036+dazyfreez@users.noreply.github.com>
2022-06-06 23:18:08 +0200

    -

--- db/-- SQLite.sql
+++ db/-- SQLite.sql
@@ -1,8 +1,8 @@
 -- SQLite
-CREATE TABLE essensplaner_db (
-    gerichtname TEXT PRIMARY KEY NOT NULL,
-    zutaten TEXT,
-    time TEXT,
-    description TEXT,
-    category TEXT,
-);
+--CREATE TABLE essensplaner_db (
+  -- gerichtname TEXT PRIMARY KEY NOT NULL,
+  --  zutaten TEXT,
+   -- time TEXT,
+ --   description TEXT,
+  --  category TEXT,
+--);
