8e14a841a1fc16cbf32edbe96450c7663c12c3db
dazyfreez <74270036+dazyfreez@users.noreply.github.com>
2022-06-06 23:21:00 +0200

    -

---
+++ db/-- SQLite1.sql
@@ -0,0 +1,9 @@
+-- SQLite
+CREATE TABLE essensplaner_db (
+  gerichtname TEXT PRIMARY KEY NOT NULL,
+  zutaten TEXT,
+   time TEXT,
+ description TEXT,
+  category TEXT,
+);
+insert into essensplaner_db (gerichtname, zutaten, time, description, category) values ('Pizza', 'Tomatensauce, Mozzarella, Basilikum, Salami', '20:00', 'Pizza mit Tomatensauce, Mozzarella, Basilikum, Salami', 'Pizza');
