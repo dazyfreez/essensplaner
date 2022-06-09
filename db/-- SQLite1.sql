4a3b0a17883ab6d76e1481025d95c0be00d3aa41
dazyfreez <74270036+dazyfreez@users.noreply.github.com>
2022-06-09 14:39:45 +0200

    -

--- db/-- SQLite1.sql
+++ db/-- SQLite1.sql
@@ -1,8 +1,8 @@
 -- SQLite
-CREATE TABLE meal (
-  gerichtname TEXT NOT NULL PRIMARY KEY ,
-  zutaten TEXT NOT NULL,
-  zeit TEXT NOT NULL,
-  beschreibung TEXT NOT NULL,
-  kategorie TEXT NOT NULL,
-);
+--CREATE TABLE meal (
+ -- gerichtname TEXT NOT NULL PRIMARY KEY ,
+ -- zutaten TEXT NOT NULL,
+ -- zeit TEXT NOT NULL,
+ -- beschreibung TEXT NOT NULL,
+  -----kategorie TEXT NOT NULL,
+--);
