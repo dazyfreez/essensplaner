12031fe977bdb4892c24a401accd177f59d268e9
dazyfreez <74270036+dazyfreez@users.noreply.github.com>
2022-06-09 14:42:38 +0200

    -

--- db/-- SQLite1.sql
+++ db/-- SQLite1.sql
@@ -1,4 +1,4 @@
 -- SQLite
-CREATE TABLE meal (gerichtname TEXT NOT NULL PRIMARY KEY , zutaten TEXT , zeit TEXT , beschreibung TEXT , kategorie TEXT ,);
+--CREATE TABLE meal (gerichtname TEXT NOT NULL PRIMARY KEY , zutaten TEXT , zeit TEXT , beschreibung TEXT , kategorie TEXT ,);
 INSERT INTO meal (gerichtname, zutaten, zeit, beschreibung, kategorie) 
 VALUES ('Pizza', 'Tomatensauce, Mozzarella, Basilikum, Salami', '20:00', 'Pizza mit Tomatensauce, Mozzarella, Basilikum, Salami', 'Pizza');
