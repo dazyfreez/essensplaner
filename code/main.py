19e7f2669713b75ad51790ab7d52f469017c1fbb
dazyfreez <dazyfreez@gmail.com>
2022-06-12 01:09:38 +0200

    -

--- code/main.py
+++ code/main.py
@@ -1,6 +1,9 @@
 from multiprocessing import connection, Process
 from time import sleep
 from random import randint
+import sys
+import os
+import time
 print("das ist das mainscript") 
 essen =  ["spagetti", "burger", "lasagne", "bestellen", "eier"]
 print("dieses skript ist ein essensplaner")
