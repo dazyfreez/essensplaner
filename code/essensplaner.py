8e3afe9d57172080eb44f463ebe3a5033d5a8df4
dazyfreez <dazyfreez@gmail.com>
2022-06-12 01:03:33 +0200

    -

--- code/essensplaner.py
+++ code/essensplaner.py
@@ -1,5 +1,3 @@
-import mysql
-import mysql.connector
 print("das ist ein essensplaner")
 essen =  ["spagetti", "burger", "lasagne", "bestellen", "eier"]
 x = int(input("für wie viel tage wollen sie planen"))
