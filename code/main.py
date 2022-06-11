00a32f614f69c9fddc6cce0326df60b6a11f2cee
dazyfreez <dazyfreez@gmail.com>
2022-06-12 01:10:56 +0200

    -

--- code/main.py
+++ code/main.py
@@ -8,30 +8,3 @@ print("das ist das mainscript")
 essen =  ["spagetti", "burger", "lasagne", "bestellen", "eier"]
 print("dieses skript ist ein essensplaner")
 print("es soll zukünftig mit der datenbank zusammen arbeiten")
-connection = mysql.connector.connect(host="localhost", user="root", passwd="", database="essen")
-cursor = connection.cursor()
-cursor.execute("SELECT * FROM essen")
-for row in cursor:
-    print(row)
-print("wollen sie das menü öffen")
-print("ja oder nein")
-eingabe = input()
-if eingabe == "ja":
-    print("1 führ eine andere reinfolge aus")
-    print("2 führ einen neuen eintrag hinzu")
-    print("3 lösche einen eintrag")
-    print("4 ändere einen eintrag")
-    print("5 speichere die ergebnisse in einer datei")
-    print("6 beenden")
-    eingabe = int(input())
-    if eingabe == 1:
-        essen.reverse()
-        print(essen)
-    elif eingabe == 2:
-        neu = input("was möchten sie hinzufügen")
-        essen.append(neu)
-        print(essen)
-    elif eingabe == 3:
-        löschen = input("was möchten sie löschen")
-        essen.remove(löschen)
-        print(essen)
