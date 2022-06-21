a1c866110c9e36efd0048d74491c4f8effe25360
dazyfreez <74270036+dazyfreez@users.noreply.github.com>
2022-06-21 07:48:13 +0200

    -

--- code/sql1.py
+++ code/sql1.py
@@ -1,3 +1,9 @@
+from re import I
+
+
 print("dieses skript soll mit der datenbank kommunizieren") 
 print("soll das skript ausgeführt werden")
 print("ja oder nein")
+eingabe = input()
+if eingabe == "ja":
+    
