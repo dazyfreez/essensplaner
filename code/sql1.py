a0acf4de9a91b038cc805a2745899d4cf0f5c8c7
dazyfreez <74270036+dazyfreez@users.noreply.github.com>
2022-06-23 17:45:06 +0200

    -

--- code/sql1.py
+++ code/sql1.py
@@ -4,12 +4,4 @@ from re import I
 print("dieses skript soll mit der datenbank kommunizieren") 
 print("soll das skript ausgeführt werden")
 print("ja oder nein")
-eingabe = input()
-if eingabe == "ja":
-    print("wollen sie das menü öffnen")
-    print("ja oder nein")
-    eingabe = input()
-    if eingabe == "ja":
-        print("kommt noch")
-        print("daran wird noch gearbeitet")
-
+print("")
